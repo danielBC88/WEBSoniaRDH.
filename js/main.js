763b4ea73f48a8b02d1658e3fc9d5bb181b5d399
danielBC88 <63165049+danielBC88@users.noreply.github.com>
2020-04-30 09:59:36 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+(function () {
+    var options = {
+        facebook: "225326547510896", // Facebook page ID
+        call_to_action: "", // Call to action
+        position: "left", // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
+
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
